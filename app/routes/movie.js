f10fe5a23890e6aeedec8ce04a3ae44915040ead
Daniel Yuschick <danyuschick@gmail.com>
2016-09-24 17:26:40 -0400

    Update to loading the wallpaper image more cleanly.

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -3,23 +3,26 @@ import $ from 'jquery';
 import delay from 'ember-delay/delay';
 
 export default Ember.Route.extend({
-    model(params) {
-        const self = this;
-        return delay(100).then(function() {
-            return self.store.findRecord('movies', params.movie_id);
-        });
-    },
-    afterModel(movies) {
-        if (movies.get('label')) {
-            delay(200).then(function() {
-                $('.card-sub-header').addClass('active');
-            });
-        }
-
-        delay(250).then(function() {
-            $('.wallpaper-image').addClass('active');
-            $('.card-content, .trailer-content').fadeIn(50);
-        });
-
+  loadImage: function(url) {
+    var wallpaper = new Image();
+    wallpaper.src = 'assets/images/wallpapers/' + url;
+    wallpaper.onload = function() {
+      $('.wallpaper-image').addClass('active');
+      $('.card-content, .trailer-content').fadeIn(50);
+    };
+  },
+  model(params) {
+    const self = this;
+    return delay(100).then(function() {
+      return self.store.findRecord('movies', params.movie_id);
+    });
+  },
+  afterModel(movies) {
+    if (movies.get('label')) {
+      delay(200).then(function() {
+        $('.card-sub-header').addClass('active');
+      });
     }
+    this.loadImage(movies.get('image'));
+  }
 });
